507c6c52ecd9be540c5f3b5e3dab7eba3173aeb2
Zerbaib <hugo.hennetin@proton.me>
2023-12-20 15:55:54 +0100

    comment

--- src/main.ino
+++ src/main.ino
@@ -1,18 +1,18 @@
-int ledPin = 2;
-int buttonPin = 3;
-int buttonState;
+int ledPin = 2; // Definition du pin pour la led
+int buttonPin = 3; // Definition du pin pour le button
+int buttonState; // Declaration de la variable pour l'etat du boutton
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
+    pinMode(ledPin, OUTPUT); // OUTPUT est une constante Boolein 0 ou 1
+  pinMode(buttonPin, INPUT); // INPUT est une constante Boolein 0 ou 1
 }
 
 void loop()
 {
     buttonState = digitalRead(buttonPin);
-    if (buttonState == HIGH) {
-        digitalWrite(ledPin, LOW);
+    if (buttonState == HIGH) { // HIGH est une constante Boolein qui a pour valeur 1 qui fournit du 5 V
+        digitalWrite(ledPin, LOW); // LOW est une constante Boolein qui a pour valeur 0 qui fournie pas de tension
     }
     else {
         digitalWrite(ledPin, HIGH);
